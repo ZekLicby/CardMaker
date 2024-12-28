daced33daeacf98dbdb0da77d5c8c79ec044f333
ramon <ramon.silva109@gmail.com>
2024-12-28 12:26:16 -0300

    criacao dark icon

--- src/assets/Icons/index.ts
+++ src/assets/Icons/index.ts
@@ -1,3 +1,4 @@
 export { LevelStarIcon } from "./LevelStarIcon";
 export { LightAttributeIcon } from "./LightAttributeIcon";
 export { WaterAttributeIcon } from "./WaterAttributeIcon";
+export { DarkAttributeIcon } from "./DarkAttributeIcon";
