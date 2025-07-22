fd6bb43609ebcc3f08224a4376714ab652063954
Carlos Lins <carloslinsalbertoa@gmail.com>
2025-07-22 01:49:25 -0300

    Adição de bordas em elementos da carta

--- src/components/Card/ViewCard.tsx
+++ src/components/Card/ViewCard.tsx
@@ -1,15 +1,25 @@
 import {
+  BackgroundContent,
+  CardBorder,
   CardContainer,
+  CardDetailsContainer,
   CardImage,
   CardNameContainer,
   CardNameText,
+  FrameImage,
   ImageContainer,
+  MonsterImageFrame,
   StarAndImageContainer,
   StarContainer,
 } from "./styles";
 
 import { FC } from "react";
 import { IViewCard } from "./types";
+
+import CampoImagem from "../../../public/artBox.png";
+import Borda from "../../../public/borderFull.png";
+// import Hologram from "../../../public/hologram.png";
+import BordaLore from "../../../public/loreBorder.png";
 
 export const ViewCard: FC<IViewCard> = ({
   currentCardBackground,
@@ -32,13 +42,34 @@ export const ViewCard: FC<IViewCard> = ({
 
         <ImageContainer>
           <CardImage
-            src={cardImage as string}
+            src={cardImage as unknown as string}
             width={356}
             height={288}
-            alt="Dragão branco de olhos azuis"
+            alt="Imagem desconhecida"
           />
+          <MonsterImageFrame src={CampoImagem.src as string} />
         </ImageContainer>
       </StarAndImageContainer>
+
+      {/*       ESSE AQUI DÁ PRA USAR IMAGEM DIRETA       */}
+
+      {/*       <CardImage
+        src={hologram.src as string}
+        width={356}
+        height={288}
+        alt="Dragão branco de olhos azuis"
+      />
+      */}
+
+      <CardDetailsContainer>
+        <BackgroundContent>
+          <h1>sakljhsajhglh,</h1>
+          <p>afkhjgasl</p>
+        </BackgroundContent>
+        <FrameImage src={BordaLore.src as string} />
+      </CardDetailsContainer>
+
+      <CardBorder src={Borda.src as string} />
     </CardContainer>
   );
 };
