82d84d1f84d9f4d00de1f949ff88e9f388e61a6e
Carlos Lins <carloslinsalbertoa@gmail.com>
2025-08-30 14:11:41 -0300

    Correção de regra eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,7 +15,7 @@ const eslintConfig = [
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
-      "@typescript-eslint/prefer-const": "off",
+      "prefer-const": "off"
     },
   },
 ];
